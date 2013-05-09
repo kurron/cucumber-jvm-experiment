a874f93849203bb3e04b1d58a74d5d1fa4d4a06d
Ron Kurr <kurr@kurron.org>
2013-05-08 20:31:44 -0400

    Removed the Java source and library.  You can't mix Java and Groovy because the runtime gets a bit confused.

--- src/test/groovy/org/kurron/cucumber/groovy/StepDefinitions.groovy
+++ src/test/groovy/org/kurron/cucumber/groovy/StepDefinitions.groovy
@@ -1,30 +1,26 @@
 package org.kurron.cucumber.groovy
 
-import cucumber.api.java.en.Given
-import cucumber.api.java.en.Then
-import cucumber.api.java.en.When
+import cucumber.api.PendingException
+
+
+this.metaClass.mixin(cucumber.api.groovy.Hooks)
+this.metaClass.mixin(cucumber.api.groovy.EN)
 
 /**
  * A groovy version of the test implementations.
  */
-@SuppressWarnings( "GrMethodMayBeStatic" )
-class StepDefinitions
-{
-    @Given( '^I have \\$(\\d+) in my account$' )
-    public void I_have_$_in_my_account( final int dollars )
-    {
-        System.err.println( "dollars = " + dollars );
-    }
 
-    @When( '^I wave my magic ring at the machine$' )
-    public void I_wave_my_magic_ring_at_the_machine()
-    {
-        System.err.println( "Magic ring is used." );
-    }
+Given(~'^I have \\$(\\d+) in my account$') { int arg1 ->
+    // Express the Regexp above with the code you wish you had
+    throw new PendingException()
+}
 
-    @Then( '^I should get a soda$' )
-    public void I_should_get_a_soda()
-    {
-        System.err.println( "A soda has been returned." );
-    }
+When(~'^I wave my magic ring at the machine$') { ->
+    // Express the Regexp above with the code you wish you had
+    throw new PendingException()
+}
+
+Then(~'^I should get a soda$') { ->
+    // Express the Regexp above with the code you wish you had
+    throw new PendingException()
 }
