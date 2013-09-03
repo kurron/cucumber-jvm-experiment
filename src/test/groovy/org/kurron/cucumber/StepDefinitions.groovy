9e60699162ae6be83c8a474d341c6ea240a56a81
Ron Kurr <kurr@kurron.org>
2013-09-03 16:11:53 -0400

    Even more Groovy.

--- src/test/groovy/org/kurron/cucumber/StepDefinitions.groovy
+++ src/test/groovy/org/kurron/cucumber/StepDefinitions.groovy
@@ -4,7 +4,6 @@ import cucumber.api.java.en.Given
 import cucumber.api.java.en.Then
 import cucumber.api.java.en.When
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.ResponseEntity
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.web.client.RestTemplate
 
@@ -19,7 +18,7 @@ class StepDefinitions {
     /**
      * Context passed between steps.
      **/
-    ResponseEntity<String> response
+    def response
 
     @Given( '^A valid REST template$' )
     void '^A valid REST template$'() {
