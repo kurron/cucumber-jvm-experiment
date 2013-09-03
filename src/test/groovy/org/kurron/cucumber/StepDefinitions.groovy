fe8e14bda321b9d47f8842c06d3500f673af6d6e
Ron Kurr <kurr@kurron.org>
2013-09-03 16:08:35 -0400

    Made it a bit more Groovy.

--- src/test/groovy/org/kurron/cucumber/StepDefinitions.groovy
+++ src/test/groovy/org/kurron/cucumber/StepDefinitions.groovy
@@ -4,7 +4,6 @@ import cucumber.api.java.en.Given
 import cucumber.api.java.en.Then
 import cucumber.api.java.en.When
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.web.client.RestTemplate
@@ -13,29 +12,28 @@ import org.springframework.web.client.RestTemplate
  * A Cucumber JVM test to illustrate how to integration the test with a Spring context.
  **/
 @ContextConfiguration( locations = ['classpath:cucumber.xml'] )
-class StepDefinitions
-{
+class StepDefinitions {
     @Autowired
     RestTemplate template
 
     /**
      * Context passed between steps.
      **/
-    ResponseEntity<java.lang.String> response
+    ResponseEntity<String> response
 
-    @Given('^A valid REST template$')
-    public void A_valid_REST_template() throws Throwable {
+    @Given( '^A valid REST template$' )
+    void '^A valid REST template$'() {
         assert template
     }
 
-    @When('^I invoke GET on wwww.google.com$')
-    public void I_invoke_GET_on_wwww_google_com() throws Throwable {
+    @When( '^I invoke GET on wwww.google.com$' )
+    void '^I invoke GET on wwww.google.com$'() {
         response = template.getForEntity( 'http://www.google.com/', String )
     }
 
-    @Then('^I should get a (\\d+) status code$')
-    public void I_should_get_a_status_code(int arg1) throws Throwable {
+    @Then( '^I should get a (\\d+) status code$' )
+    void '^I should get a (\\d+) status code$'( int arg1 ) {
         assert response
-        assert response.statusCode == HttpStatus.OK
+        assert response.statusCode.value() == arg1
     }
 }
